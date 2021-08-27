a187da4a7a882da5b04c060a5ae4cd022e9e0464
JonasREJCS <jj_jcs@hotmail.com>
2021-08-27 13:49:00 -0300

    added list of transactions

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('+ RS 100,00'),
+                subtitle: Text('1000'),
+              )
+            ),
+            Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('+ RS 200,00'),
+                  subtitle: Text('1200'),
+                )
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: Text(
             'Transactions',
           ),
+          backgroundColor: Colors.purpleAccent,
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
